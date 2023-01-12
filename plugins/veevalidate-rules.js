1ba97d0bb64b93497a89468e719128f40123aa47
mihul87 <mihaibotnaru87@gmail.com>
2023-01-12 21:32:56 +0200

    vee validate and yup setup

---
+++ plugins/veevalidate-rules.js
@@ -0,0 +1,11 @@
+import { defineRule } from 'vee-validate';
+import AllRules from '@vee-validate/rules';
+
+export default defineNuxtPlugin((nuxtApp) => {
+    // defineRule('required', required);
+
+    Object.keys(AllRules).forEach(rule => {
+      defineRule(rule, AllRules[rule]);
+      console.log(rule);
+    });
+})
